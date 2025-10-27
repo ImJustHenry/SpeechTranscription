bef52d5e62791a84700460a3ddd53042e411edd8
ImJustHenry <wanghenry21@gmail.com>
2025-10-27 00:12:41 -0500

    fix tqdm 5

--- GUI.py
+++ GUI.py
@@ -1,6 +1,5 @@
 # Adding Logging - CICD Internal Dev 
 import logging
-import tempfile
 
 # main.py
 from customtkinter import *
@@ -12,15 +11,15 @@ from components.error_handler import global_error_handler, show_error_popup
 from components.constants import WIDTH, HEIGHT, SETTINGS_FILE
 import os
 import sys
-import io
 
 # Logging setup - CICD Internal Dev 
-
-log_path = os.path.join(tempfile.gettempdir(), "app.log")
-
 logging.basicConfig(
-    level=logging.INFO,
-    handlers=[logging.FileHandler(log_path, mode="w")]
+    level=logging.INFO,  
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout),  # logs to console
+        logging.FileHandler("app.log", mode="w")  # logs to a file
+    ]
 )
 
 logger = logging.getLogger("SpeechTranscription")
